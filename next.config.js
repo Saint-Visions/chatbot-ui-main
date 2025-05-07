6978c8b48c93ce4a02681462e7ca1caac7c362b0
Saint-Visions <ryan@saintvisiongroup.com>
2025-05-06 19:07:16 -0700

    first commit

---
+++ next.config.js
@@ -0,0 +1,30 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true"
+})
+
+const withPWA = require("next-pwa")({
+  dest: "public"
+})
+
+module.exports = withBundleAnalyzer(
+  withPWA({
+    reactStrictMode: true,
+    images: {
+      remotePatterns: [
+        {
+          protocol: "http",
+          hostname: "localhost"
+        },
+        {
+          protocol: "http",
+          hostname: "127.0.0.1"
+        },
+        {
+          protocol: "https",
+          hostname: "**"
+        }
+      ]
+    },
+    serverExternalPackages: ["sharp", "onnxruntime-node"]
+  })
+)
