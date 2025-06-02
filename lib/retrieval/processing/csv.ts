2d6b004b29ec7c08f3b8e30267d64d81224a3fe6
Saint-Visions <ryan@saintvisiongroup.com>
2025-06-02 15:28:27 -0700

    ♻️ Remove unsupported langchain loaders and fix csv/pdf processors

--- lib/retrieval/processing/csv.ts
+++ lib/retrieval/processing/csv.ts
@@ -1,26 +1,22 @@
 import { FileItemChunk } from "@/types"
 import { encode } from "gpt-tokenizer"
-import { CSVLoader } from "langchain/document_loaders/fs/csv"
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
 import { CHUNK_OVERLAP, CHUNK_SIZE } from "."
 
 export const processCSV = async (csv: Blob): Promise<FileItemChunk[]> => {
-  const loader = new CSVLoader(csv)
-  const docs = await loader.load()
-  let completeText = docs.map(doc => doc.pageContent).join("\n\n")
+  const text = await csv.text()
 
   const splitter = new RecursiveCharacterTextSplitter({
     chunkSize: CHUNK_SIZE,
     chunkOverlap: CHUNK_OVERLAP,
     separators: ["\n\n"]
   })
-  const splitDocs = await splitter.createDocuments([completeText])
 
-  let chunks: FileItemChunk[] = []
+  const splitDocs = await splitter.createDocuments([text])
 
-  for (let i = 0; i < splitDocs.length; i++) {
-    const doc = splitDocs[i]
+  const chunks: FileItemChunk[] = []
 
+  for (const doc of splitDocs) {
     chunks.push({
       content: doc.pageContent,
       tokens: encode(doc.pageContent).length
