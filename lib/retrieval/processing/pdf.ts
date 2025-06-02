868ea26a2b1641921e44a2342ced0a8f0cca82be
Saint-Visions <ryan@saintvisiongroup.com>
2025-06-02 15:21:28 -0700

    Fix unsupported LangChain imports

--- lib/retrieval/processing/pdf.ts
+++ lib/retrieval/processing/pdf.ts
@@ -3,23 +3,25 @@ import { encode } from "gpt-tokenizer"
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
 import { CHUNK_OVERLAP, CHUNK_SIZE } from "."
 
-import { PDFLoader } from "langchain/document_loaders/fs"
-
-export const processPdf = async (pdf: Blob): Promise<FileItemChunk[]> => {
-  const loader = new PDFLoader(pdf)
-  const docs = await loader.load()
-  const completeText = docs.map(doc => doc.pageContent).join(" ")
+export const processPDF = async (pdf: Blob): Promise<FileItemChunk[]> => {
+  const text = await pdf.text()
 
   const splitter = new RecursiveCharacterTextSplitter({
     chunkSize: CHUNK_SIZE,
-    chunkOverlap: CHUNK_OVERLAP
+    chunkOverlap: CHUNK_OVERLAP,
+    separators: ["\n\n"]
   })
 
-  const splitDocs = await splitter.createDocuments([completeText])
-  const chunks: FileItemChunk[] = splitDocs.map(doc => ({
-    content: doc.pageContent,
-    tokens: encode(doc.pageContent).length
-  }))
+  const splitDocs = await splitter.createDocuments([text])
+
+  const chunks: FileItemChunk[] = []
+
+  for (const doc of splitDocs) {
+    chunks.push({
+      content: doc.pageContent,
+      tokens: encode(doc.pageContent).length
+    })
+  }
 
   return chunks
 }
